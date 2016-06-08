6c1850036a71d28f715452212d28d0cbb96845a8
adminsoyoda <lcrespin@soyoda.com>
2016-06-08 16:36:29 -0500

    Actualización
    Ip Pública

--- js/SAC.js
+++ js/SAC.js
@@ -1,6 +1,6 @@
 
 //var server = "http://192.168.2.41:81/SAC";
-var server = "http://sac.intranet.cl:82/SAC_Intranet";
+var server = "http://200.7.214.192:82/SAC_Intranet";
 var controller = "";
 var fieldHide = "";
 
